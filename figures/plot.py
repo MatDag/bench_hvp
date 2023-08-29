b4db04f267cb53a8e3c4763fa7ee40da2fd48f26
MatDag <mathieu.dagreou@inria.fr>
2023-08-29 18:05:20 +0200

    CLN no memory plot

--- figures/plot.py
+++ figures/plot.py
@@ -3,14 +3,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 LEGEND_RATIO = 0.1
-DEFAULT_WIDTH = 6
+DEFAULT_WIDTH = 5
 DEFAULT_DOUBLE_WIDTH = 6.75
-DEFAULT_HEIGHT = 3.
-
-METRICS = dict(
-    time="Time [sec]",
-    memory="Memory [MB]"
-)
+DEFAULT_HEIGHT = 3
 
 STYLES = {
     'Gradient': '#5778a4',
@@ -30,41 +25,40 @@ mpl.rcParams.update({
 df = pd.read_parquet('../outputs/bench_hvp.parquet')
 
 fig = plt.figure(
-    figsize=(DEFAULT_WIDTH, DEFAULT_HEIGHT * (1 + LEGEND_RATIO))
+    figsize=(DEFAULT_WIDTH, DEFAULT_HEIGHT)
 )
-gs = plt.GridSpec(2, 3, width_ratios=[1, .1, 1], height_ratios=[.1, 1])
-axes = [fig.add_subplot(gs[1, i]) for i in [0, 2]]
+gs = plt.GridSpec(1, 2, width_ratios=[.6, .4], hspace=0)
+ax = fig.add_subplot(gs[0, 0])
 
-for ax, metric in zip(axes, METRICS):
-    lines = []
+lines = []
 
-    for label in df['label'].unique():
-        to_plot = (
-            df.query("label == @label")
-            .groupby(['depth'])
-            .quantile([0.2, 0.5, 0.8], numeric_only=True)
-            .unstack()
-        )
-        lines.append(
-            ax.plot(
-                to_plot.index,
-                to_plot.loc[:, (metric.lower(), 0.5)],
-                color=STYLES[label],
-                label=label
-            )[0]
-        )
-        ax.fill_between(
+for label in df['label'].unique():
+    to_plot = (
+        df.query("label == @label")
+        .groupby(['depth'])
+        .quantile([0.2, 0.5, 0.8], numeric_only=True)
+        .unstack()
+    )
+    lines.append(
+        ax.plot(
             to_plot.index,
-            to_plot.loc[:, (metric.lower(), 0.2)],
-            to_plot.loc[:, (metric.lower(), 0.8)],
-            alpha=0.3,
-            color=STYLES[label]
-        )
-    ax.set_xlabel('Depth')
-    ax.set_ylabel(METRICS[metric])
+            to_plot.loc[:, ("time", 0.5)],
+            color=STYLES[label],
+            label=label
+        )[0]
+    )
+    ax.fill_between(
+        to_plot.index,
+        to_plot.loc[:, ("time", 0.2)],
+        to_plot.loc[:, ("time", 0.8)],
+        alpha=0.3,
+        color=STYLES[label]
+    )
+ax.set_xlabel('Depth')
+ax.set_ylabel('Time [sec]')
 
-ax_legend = fig.add_subplot(gs[0, :])
+ax_legend = fig.add_subplot(gs[0, 1])
 ax_legend.set_axis_off()
-ax_legend.legend(handles=lines, loc='center', ncol=2)
+ax_legend.legend(handles=lines, loc='center', ncol=1)
 
 plt.savefig('bench_hvp_test.pdf', dpi=300)
