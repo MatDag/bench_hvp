fb9defe2dc1baf8ed719a4ad6ef8a925763bd89f
MatDag <mathieu.dagreou@inria.fr>
2023-10-12 11:46:18 +0200

    ENH figures

--- figures/plot_resnet_wrt_depth.py
+++ figures/plot_resnet_wrt_depth.py
@@ -1,11 +1,16 @@
+import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 BATCH_SIZE = 128
 
+LEGEND_INSIDE = True
 LEGEND_RATIO = 0.1
-DEFAULT_WIDTH = 7
+if LEGEND_INSIDE:
+    DEFAULT_WIDTH = 4.6
+else:
+    DEFAULT_WIDTH = 7.25
 DEFAULT_DOUBLE_WIDTH = 6.75
 DEFAULT_HEIGHT = 3
 
@@ -47,12 +52,21 @@ for model in MODELS:
 fig = plt.figure(
     figsize=(DEFAULT_WIDTH, DEFAULT_HEIGHT)
 )
-gs = plt.GridSpec(1, 2, width_ratios=[.6, .4], hspace=0, bottom=.1)
-ax = fig.add_subplot(gs[0, 0])
+if LEGEND_INSIDE:
+    ax = ax = fig.add_subplot()
+else:
+    gs = plt.GridSpec(1, 2, width_ratios=[.65, .35], hspace=0, bottom=.1)
+    ax = fig.add_subplot(gs[0, 0])
 
 lines = []
 
-for fun in df['label'].unique():
+width = .20
+space = .1
+n_funs = len(df['label'].unique())
+mutliplier = np.arange(n_funs)
+x = np.arange(len(MODELS)) * (1 + space)
+
+for i, fun in enumerate(df['label'].unique()):
     to_plot = (
         df.query("label == @fun")
         .groupby(['model'])
@@ -60,28 +74,29 @@ for fun in df['label'].unique():
         .unstack().sort_values(by=('ord', 0.5))
     )
     lines.append(
-        ax.plot(
-            to_plot.index,
+        ax.bar(
+            x + mutliplier[i] * width,
             to_plot.loc[:, ("time", 0.5)],
+            width=width,
+            yerr=[
+                to_plot.loc[:, ("time", 0.5)] - to_plot.loc[:, ("time", 0.2)],
+                to_plot.loc[:, ("time", 0.8)] - to_plot.loc[:, ("time", 0.5)]
+            ],
             color=STYLES[fun]['color'],
             label=STYLES[fun]['label'],
-            marker='o'
-        )[0]
+        )
     )
-    ax.fill_between(
-        to_plot.index,
-        to_plot.loc[:, ("time", 0.2)],
-        to_plot.loc[:, ("time", 0.8)],
-        alpha=0.3,
-        color=STYLES[fun]['color']
-    )
+
 ax.set_ylabel('Time [sec]')
 # ax.set_yscale('log')
-ax.set_xticks(ticks=to_plot.index,
+ax.set_xticks(ticks=x + 2*width,
               labels=[MODELS[m]['label'] for m in MODELS])
 
-ax_legend = fig.add_subplot(gs[0, 1])
-ax_legend.set_axis_off()
-ax_legend.legend(handles=lines, loc='center', ncol=1)
+if LEGEND_INSIDE:
+    ax_legend = ax.legend(ncol=1)
+else:
+    ax_legend = fig.add_subplot(gs[0, 1])
+    ax_legend.set_axis_off()
+    ax_legend.legend(handles=lines, loc='center', ncol=1)
 
 plt.savefig('bench_hvp_resnet_wrt_depth.pdf', dpi=300)
