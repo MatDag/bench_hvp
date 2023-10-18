e1c0b4dec079cf3464cc45a719ca7f53b0427765
MatDag <mathieu.dagreou@inria.fr>
2023-10-18 11:38:36 +0200

    ENH torch implementation for tanh

--- bench_hvp/bench_tanh.py
+++ bench_hvp/bench_tanh.py
@@ -1,6 +1,8 @@
 import jax
 import jax.numpy as jnp
 
+import torch
+
 import pandas as pd
 from functools import partial
 
@@ -22,15 +24,29 @@ DIM_LIST = jnp.logspace(1, 5, 14, dtype=jnp.int32)
 SLURM_CONFIG = 'config/slurm_cpu.yml'
 
 
-def f(x):
+@jax.jit
+def f_jax(x):
     return jnp.sum(jnp.tanh(x)**2)
 
 
+@torch.compile
+def f_torch(x):
+    return torch.sum(torch.tanh(x)**2)
+
+
 @mem.cache
-def run_one(fun_name, dim=1, n_reps=1):
-    key = jax.random.PRNGKey(0)
-    x = jax.random.normal(key, (dim, ))
-    grad_fun = jax.jit(jax.grad(f))
+def run_one(fun_name, framework='jax', dim=1, n_reps=1):
+    if framework == 'jax':
+        key = jax.random.PRNGKey(0)
+        x = jax.random.normal(key, (dim, ))
+        fun = f_jax
+        grad_fun = jax.jit(jax.grad(fun))
+    elif framework == 'torch':
+        gen = torch.Generator()
+        gen.manual_seed(0)
+        x = torch.randn(dim, generator=gen)
+        fun = f_torch
+        grad_fun = torch.func.grad(fun)
 
     if fun_name == "grad":
         grad_fun(x)  # First run for compilation
@@ -93,49 +109,88 @@ def run_bench(fun_list, dim_list, n_reps, slurm_config_path=SLURM_CONFIG):
 
 
 @jax.jit
-def hvp_naive(x, v):
+def hvp_naive_jax(x, v):
     """
     Returns the Hessian-vector product by computing the full Hessian matrix
     and then multiplying by the tangent vector v.
     """
 
-    return jnp.dot(jax.hessian(f)(x), v)
+    return jnp.dot(jax.hessian(f_jax)(x), v)
+
+
+def hvp_naive_torch(x, v):
+    """
+    Returns the Hessian-vector product by computing the full Hessian matrix
+    and then multiplying by the tangent vector v.
+    """
+
+    return torch.dot(torch.func.hessian(f_torch)(x), v)
 
 
 @jax.jit
-def hvp_forward_over_reverse(x, v):
+def hvp_forward_over_reverse_jax(x, v):
     """
     Returns the Hessian-vector product by forward-over-reverse propagation.
     """
-    return jax.jvp(jax.grad(f), (x, ), (v, ))[1]
+    return jax.jvp(jax.grad(f_jax), (x, ), (v, ))[1]
+
+
+def hvp_forward_over_reverse_torch(x, v):
+    """
+    Returns the Hessian-vector product by forward-over-reverse propagation.
+    """
+    return torch.func.jvp(torch.func.grad(f_torch), (x, ), (v, ))[1]
 
 
 @jax.jit
-def hvp_reverse_over_forward(x, v):
+def hvp_reverse_over_forward_jax(x, v):
     """
     Returns the Hessian-vector product by reverse-over-forward propagation.
     """
     def jvp_fun(x, v):
-        return jax.jvp(f, (x, ), (v, ))[1]
+        return jax.jvp(f_jax, (x, ), (v, ))[1]
+
+    return jax.grad(jvp_fun)(x, v)
+
+
+def hvp_reverse_over_forward_torch(x, v):
+    """
+    Returns the Hessian-vector product by reverse-over-forward propagation.
+    """
+    def jvp_fun(x, v):
+        return torch.func.jvp(f_torch, (x, ), (v, ))[1]
 
     return jax.grad(jvp_fun)(x, v)
 
 
 @jax.jit
-def hvp_reverse_over_reverse(x, v):
+def hvp_reverse_over_reverse_jax(x, v):
     """
     Returns the Hessian-vector product by reverse-over-reverse propagation.
     """
-    return jax.grad(lambda x: jnp.dot(jax.grad(f)(x), v))(x)
+    return jax.grad(lambda x: jnp.dot(jax.grad(f_jax)(x), v))(x)
+
+
+def hvp_reverse_over_reverse_torch(x, v):
+    """
+    Returns the Hessian-vector product by reverse-over-reverse propagation.
+    """
+    return torch.func.grad(
+        lambda x: torch.dot(torch.func.grad(f_torch)(x), v)
+    )(x)
 
 
 if __name__ == '__main__':
     fun_dict = dict(
         # grad=None,
-        hvp_naive=hvp_naive,
-        hvp_forward_over_reverse=hvp_forward_over_reverse,
-        hvp_reverse_over_forward=hvp_reverse_over_forward,
-        hvp_reverse_over_reverse=hvp_reverse_over_reverse,
+        hvp_naive_jax=hvp_naive_jax,
+        hvp_forward_over_reverse_jax=hvp_forward_over_reverse_jax,
+        hvp_reverse_over_forward_jax=hvp_reverse_over_forward_jax,
+        hvp_reverse_over_reverse_jax=hvp_reverse_over_reverse_jax,
+        hvp_naive_torch=hvp_naive_torch,
+        hvp_forward_over_reverse_torch=hvp_forward_over_reverse_torch,
+        hvp_reverse_over_forward_torch=hvp_reverse_over_forward_torch,
+        hvp_reverse_over_reverse_torch=hvp_reverse_over_reverse_torch,
     )
     fun_list = fun_dict.keys()
     df = run_bench(fun_list, DIM_LIST, n_reps=N_REPS,
