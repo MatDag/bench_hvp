47164308980d9201e493f428b30bcb106d979016
MatDag <mathieu.dagreou@inria.fr>
2023-12-01 15:29:33 +0100

    WIP

--- bench_hvp/bench_hvp_time.py
+++ bench_hvp/bench_hvp_time.py
@@ -33,7 +33,7 @@ from joblib import Memory
 mem = Memory(location='__cache__')
 
 N_REPS = 30
-BATCH_SIZE_LIST = [16, 32, 64, 128]
+BATCH_SIZE_LIST = [1, 2, 4, 8, 16, 32, 64, 128]
 MODEL_DICT = dict(
     resnet50_flax=dict(module=FlaxResNetForImageClassification,
                        model="microsoft/resnet-50", framework="jax",
@@ -174,10 +174,9 @@ def run_one(fun_name, model_name, batch_size=16, n_reps=1):
         params = dict(model.named_parameters())
 
         def grad_fun(x):
-            def f(x):
-                return loss_fn_torch(x, model, batch)
-            output = f(x)
-            return torch.autograd.grad(output, tuple(params.values()))
+            def f(y):
+                return loss_fn_torch(y, model, batch)
+            return torch.func.grad(f)(x)
 
     # We compute the quantities a first time for computation
     if fun_name == "grad":
@@ -243,12 +242,11 @@ def run_bench(fun_list, model_list, n_reps, batch_size_list,
 
     executor = submitit.AutoExecutor("bench_hvp_log")
     executor.update_parameters(**config)
-    skip = [(64, 'resnet50_torch'), (128, 'resnet50_torch'),
-            (64, 'resnet101_torch'), (128, 'resnet101_torch'),
-            (64, 'resnet152_torch'), (128, 'resnet152_torch'),
-            (128, 'vit_torch'), (128, 'vit_flax'),
-            (64, 'vit_torch'), (32, 'vit_torch'),
-            (64, 'bert_torch'), (128, 'bert_torch')]
+    # skip = [(64, 'resnet50_torch'), (128, 'resnet50_torch'),
+    #         (128, 'vit_torch'), (128, 'vit_flax'),
+    #         (64, 'vit_torch'), (32, 'vit_torch'),
+    #         (64, 'bert_torch'), (128, 'bert_torch')]
+    skip = []
 
     with executor.batch():
         jobs = [
@@ -258,17 +256,16 @@ def run_bench(fun_list, model_list, n_reps, batch_size_list,
                             batch_size)
             for fun_name, model_name, batch_size
             in itertools.product(fun_list, model_list, batch_size_list)
-            if ((batch_size, model_name) not in skip
-                and fun_name != "hvp_reverse_over_reverse")
+            if ((batch_size, model_name) not in skip)
         ]
 
-        jobs += [
-            executor.submit(run,
-                            "hvp_reverse_over_reverse",
-                            model_name,
-                            32)
-            for model_name in model_list if (32, model_name) not in skip
-        ]
+        # jobs += [
+        #     executor.submit(run,
+        #                     "hvp_reverse_over_reverse",
+        #                     model_name,
+        #                     32)
+        #     for model_name in model_list if (32, model_name) not in skip
+        # ]
 
     print(f"First job ID: {jobs[0].job_id}")
 
@@ -304,7 +301,6 @@ def hvp_forward_over_reverse(model, batch, num_classes=1000, framework="jax"):
             return torch.func.grad(f)(x)
 
         def hvp_fun(x, v):
-            v = {k: value for k, value in zip(x.keys(), v)}
             return torch.func.jvp(grad_fun, (x, ), (v, ))[1]
     return hvp_fun
 
@@ -330,11 +326,10 @@ def hvp_reverse_over_forward(model, batch, num_classes=1000, framework="jax"):
             return loss_fn_torch(x, model, batch)
 
         def jvp_fun(x, v):
-            return torch.func.jvp(lambda y: f(y),
+            return torch.func.jvp(f,
                                   (x, ), (v, ))[1]
 
         def hvp_fun(x, v):
-            v = {k: value for k, value in zip(x.keys(), v)}
             return torch.func.grad(jvp_fun)(x, v)
 
     return hvp_fun
@@ -351,7 +346,7 @@ def hvp_reverse_over_reverse(model, batch, num_classes=1000, framework="jax"):
                                             num_classes=num_classes)
         )
         hvp_fun = jax.jit(
-            lambda x, v: jax.grad(lambda x: utils.tree_dot(grad_fun(x), v))(x)
+            lambda x, v: jax.grad(lambda y: utils.tree_dot(grad_fun(y), v))(x)
         )
     elif framework == 'torch':
         def f(x):
@@ -361,41 +356,12 @@ def hvp_reverse_over_reverse(model, batch, num_classes=1000, framework="jax"):
             return torch.func.grad(loss_fn_torch)(x, model, batch)
 
         def hvp_fun(x, v):
-            v = {k: value for k, value in zip(x.keys(), v)}
-            return torch.func.grad(lambda x: sum(
+            return torch.func.grad(lambda y: sum(
                 torch.dot(a.ravel(), b.ravel())
-                for a, b in zip(grad_fun(x).values(), v.values()))
+                for a, b in zip(grad_fun(y).values(), v.values()))
             )(x)
 
     return hvp_fun
-
-
-def torch_hvp(model, batch, num_classes=1000, framework="torch"):
-    """
-    Returns the Hessian-vector product operator that uses reverse-over-reverse
-    propagation.
-    """
-    keys = dict(model.named_parameters()).keys()
-
-    def f(*x):
-        return loss_fn_torch({k: v for k, v in zip(keys, x)}, model, batch)
-
-    return lambda x, v: torch.autograd.functional.vhp(f, tuple(x.values()),
-                                                      v=v)[1]
-
-
-def torch_vhp(model, batch, num_classes=1000, framework="torch"):
-    """
-    Returns the Hessian-vector product operator that uses reverse-over-reverse
-    propagation.
-    """
-    keys = dict(model.named_parameters()).keys()
-
-    def f(*x):
-        return loss_fn_torch({k: v for k, v in zip(keys, x)}, model, batch)
-
-    return lambda x, v: torch.autograd.functional.vhp(f, tuple(x.values()),
-                                                      v=v)[1]
 
 
 if __name__ == '__main__':
@@ -416,11 +382,6 @@ if __name__ == '__main__':
         hvp_reverse_over_forward=hvp_reverse_over_forward,
         hvp_reverse_over_reverse=hvp_reverse_over_reverse,
     )
-    if framework == "torch":
-        fun_dict.update(
-            torch_hvp=torch_hvp,
-            torch_vhp=torch_vhp,
-        )
     model_list = [k for k in MODEL_DICT.keys()
                   if MODEL_DICT[k]['framework'] == framework]
     fun_list = fun_dict.keys()
