7883b0dfa1d3972abc5d43dc63bd1fa64cd102d8
MatDag <mathieu.dagreou@inria.fr>
2023-12-11 17:34:58 +0100

    FIX enable large batch size for vit/bert in torch

--- bench_hvp/bench_hvp_memory.sh
+++ bench_hvp/bench_hvp_memory.sh
@@ -18,15 +18,15 @@ do
     python bench_hvp_memory.py -b batch_size -m resnet34_torch -f torch --fun hvp_reverse_over_reverse
 done
 
-python bench_hvp_memory.py -b 16 -m bert_torch -f torch --fun grad
-python bench_hvp_memory.py -b 16 -m bert_torch -f torch --fun hvp_forward_over_reverse
-python bench_hvp_memory.py -b 16 -m bert_torch -f torch --fun hvp_reverse_over_forward
-python bench_hvp_memory.py -b 16 -m bert_torch -f torch --fun hvp_reverse_over_reverse
+python bench_hvp_memory.py -b 128 -m bert_torch -f torch --fun grad
+python bench_hvp_memory.py -b 128 -m bert_torch -f torch --fun hvp_forward_over_reverse
+python bench_hvp_memory.py -b 128 -m bert_torch -f torch --fun hvp_reverse_over_forward
+python bench_hvp_memory.py -b 128 -m bert_torch -f torch --fun hvp_reverse_over_reverse
 
-python bench_hvp_memory.py -b 16 -m vit_torch -f torch --fun grad
-python bench_hvp_memory.py -b 16 -m vit_torch -f torch --fun hvp_forward_over_reverse
-python bench_hvp_memory.py -b 16 -m vit_torch -f torch --fun hvp_reverse_over_forward
-python bench_hvp_memory.py -b 16 -m vit_torch -f torch --fun hvp_reverse_over_reverse
+python bench_hvp_memory.py -b 128 -m vit_torch -f torch --fun grad
+python bench_hvp_memory.py -b 128 -m vit_torch -f torch --fun hvp_forward_over_reverse
+python bench_hvp_memory.py -b 128 -m vit_torch -f torch --fun hvp_reverse_over_forward
+python bench_hvp_memory.py -b 128 -m vit_torch -f torch --fun hvp_reverse_over_reverse
 
 conda deactivate
 conda activate bench_hvp_jax
@@ -49,9 +49,9 @@ XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python
 XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m bert_flax -f jax --fun hvp_reverse_over_forward
 XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m bert_flax -f jax --fun hvp_reverse_over_reverse
 
-XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 64 -m vit_flax -f jax --fun grad
-XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 64 -m vit_flax -f jax --fun hvp_forward_over_reverse
-XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 64 -m vit_flax -f jax --fun hvp_reverse_over_forward
-XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 64 -m vit_flax -f jax --fun hvp_reverse_over_reverse
+XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m vit_flax -f jax --fun grad
+XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m vit_flax -f jax --fun hvp_forward_over_reverse
+XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m vit_flax -f jax --fun hvp_reverse_over_forward
+XLA_PYTHON_CLIENT_PREALLOCATE=false XLA_PYTHON_CLIENT_ALLOCATOR=platform  python bench_hvp_memory.py -b 128 -m vit_flax -f jax --fun hvp_reverse_over_reverse
 
 conda deactivate
